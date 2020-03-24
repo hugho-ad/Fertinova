eaaec79e9bb6ebfe59c6cedb1fead94ef08a4c94
Sebastian Ayala Mendez <sebastian-mendez@outlook.com>
2020-03-24 10:22:35 -0600

    [ERR] Fallas en módulo de fetinova_addons para las personalizaciones de Transferencias y sus cuentas analíticas en los asientos contables

--- fertinova_addons/models/stock_move_inherited.py
+++ fertinova_addons/models/stock_move_inherited.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
-from odoo.addons import decimal_precision as dp
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+import logging
+logger = logging.getLogger(__name__)
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     #########################################################
     # MODEL FIELDS
@@ -14,39 +17,27 @@ class StockMove(models.Model):
                                           index=True, 
                                           store=True, 
                                           translate=True
-                                         )
+                                         )                               
+
 
     #########################################################
     # MODEL METHODS
-    ######################################################### 
-    def _create_account_move_line(self, credit_account_id, debit_account_id, journal_id):
-        '''This methods intends to modify the original biusiness logic of the
-           _create_account_move_line method in file addons/stock_account/stock.py'''
-        self.ensure_one()
-        AccountMove = self.env['account.move']
-        StockMove = self.env['stock.move']
-        quantity = self.env.context.get('forced_quantity', StockMove.product_qty)
-        quantity = quantity if StockMove._is_in() else -1 * quantity
+    #########################################################
+    def _generate_valuation_lines_data(self, partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id):
+        """This method intends to add the value of analytic account 
+           from Tranfers into account journal"""
+        
+        #Retrieve original dictionary:        
+        res = super()._generate_valuation_lines_data(self, partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id)
+        #res = super(StockMove, self)._generate_valuation_lines_data(self, partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id)
+        #res = dict(StockMove._generate_valuation_lines_data(self, partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id))
+        #res = super(StockMove._generate_valuation_lines_data(self, partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id))
+        #res = super(StockMove, self)._generate_valuation_lines_data()
 
-        # Make an informative `ref` on the created account move to differentiate between classic
-        # movements, vacuum and edition of past moves.
-        ref = StockMove.picking_id.name
-        if self.env.context.get('force_valuation_amount'):
-            if self.env.context.get('forced_quantity') == 0:
-                ref = 'Revaluation of %s (negative inventory)' % ref
-            elif self.env.context.get('forced_quantity') is not None:
-                ref = 'Correction of %s (modification of past move)' % ref
-
-        move_lines = self.with_context(forced_ref=ref)._prepare_account_move_line(quantity, abs(StockMove.value), credit_account_id, debit_account_id)
-        if move_lines:
-            date = self._context.get('force_period_date', fields.Date.context_today(self))
-            new_account_move = AccountMove.sudo().create({
-                'journal_id': journal_id,
-                'line_ids': move_lines,
-                'date': date,
-                'ref': ref,
-                'stock_move_id': StockMove.id,
-                'analytic_account_id': self.analytic_account_id
-            })
-            new_account_move.post() 
-        return super(StockMove.StockMove, self)._create_account_move_line()
+        #Update the value of analytic account in credit and debit lines:
+        #analytic_account_id_obtained = res.mapped(self.analytic_account_id.id)
+        logger.info("Info in res: %s", res)
+        print("Info in res: %s", res)
+        res['debit_line_vals'].update({'analytic_account_id': self.analytic_account_id})
+        res['credit_line_vals'].update({'analytic_account_id': self.analytic_account_id})
+        return res, super(StockMove, self)._generate_valuation_lines_data(self, partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id)
