69c6f378cff5eadf5d018e157ae8cfddbf6cc8f8
Sebastian-AM-Wobin <smendez@wobin.com.mx>
2020-03-24 17:50:02 -0600

    [ERR] Fallas en módulo de fetinova_addons para las personalizaciones de Transferencias y sus cuentas analíticas en los asientos contables

--- fertinova_addons/__manifest__.py
+++ fertinova_addons/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "fertinova_addons",
-    'summary': """Fertinova Addons""",
+    'summary': """Custom addons for enterprise Fertinova""",
     'description': """Módulo que gestiona las distintas personalizaciones que requiere la empresa FERTINOVA en su gestión de Odoo""",
     'author': "Sebastian Ayala Mendez",
     'website': "http://www.fertinova.com",
