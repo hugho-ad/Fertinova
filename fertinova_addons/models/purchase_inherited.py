e2943e1120a37af1a61d6e02c52bc4ea23ba58d5
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-04-14 13:56:52 -0500

    [ERR]  Commit que busca enmendar cambios aún no probados

--- fertinova_addons/models/purchase_inherited.py
+++ fertinova_addons/models/purchase_inherited.py
@@ -14,45 +14,46 @@ class PurchaseOrder(models.Model):
     ##########################################################
     @api.onchange('order_line')
     def onchange_validate_price_unit(self):
+        pass
         #Obtain the current purchase order id:
-        purchase_order_id = self._origin.id  
+        #purchase_order_id = self._origin.id  
         #Object of model 'purchase.order.line':
-        purchase_order_obj = self.env['purchase.order'].browse(purchase_order_id)               
+        #purchase_order_obj = self.env['purchase.order'].browse(purchase_order_id)               
         #Retrieve the ids of the relatives lines belonging to the main purchase order:
-        purchase_order_lines = self.env['purchase.order.line'].search([('order_id', '=', purchase_order_id)])            
+        #purchase_order_lines = self.env['purchase.order.line'].search([('order_id', '=', purchase_order_id)])            
         
         #Obtain the currency of purchase order:
-        currency = purchase_order_obj.search([('id', '=', purchase_order_id)]).currency_id.id
+        #currency = purchase_order_obj.search([('id', '=', purchase_order_id)]).currency_id.id
 
         #Validate if the currency of purchase order is not Mexican Peso (MXN):
-        if currency != 33: #MXN has the id 33 in model res.currency                   
+        #if currency != 33: #MXN has the id 33 in model res.currency                   
             #If the currency is different to MXN, perform conversion:
-            sql_query = """SELECT rate FROM res_currency_rate WHERE id = %s;"""
-            self.env.cr.execute(sql_query, (currency,))
-            rate_aux = self.env.cr.fetchone()
-            rate = rate_aux[0] #Store rate or conversion factor
-            conversion_factor = 1 / rate             
+            #sql_query = """SELECT rate FROM res_currency_rate WHERE id = %s;"""
+            #self.env.cr.execute(sql_query, (currency,))
+            #rate_aux = self.env.cr.fetchone()
+            #rate = rate_aux[0] #Store rate or conversion factor
+            #conversion_factor = 1 / rate             
 
         #Validation in order to avoid purchases when price unit is lesser than sale price:
-        for value in purchase_order_lines.ids:
+        #for value in purchase_order_lines.ids:
             #Retrieve "sale price" from table 'product.template':                   
-            sql_query = """SELECT list_price FROM product_template WHERE id = %s;"""
-            self.env.cr.execute(sql_query, (value,))
-            sale_price_aux = self.env.cr.fetchone()
-            sale_price = sale_price_aux[0]        
+            #sql_query = """SELECT list_price FROM product_template WHERE id = %s;"""
+            #self.env.cr.execute(sql_query, (value,))
+            #sale_price_aux = self.env.cr.fetchone()
+            #sale_price = sale_price_aux[0]        
 
             #Conversion to Mexican Pesos (MXN) of sale price:
-            if currency != 33:
+            #if currency != 33:
                 #It is necessary to calculate the new value of currency:
-                sale_price = float(sale_price) * float(conversion_factor)
+            #    sale_price = float(sale_price) * float(conversion_factor)
                                 
             #Get price_unit of each line from purchase order line:
-            price_unit = self.env['purchase.order.line'].search([('id', '=', value)]).price_unit 
+            #price_unit = self.env['purchase.order.line'].search([('id', '=', value)]).price_unit 
             
             #Validate if sale_price is lesser than purchase price unit, then arise error:         
-            if sale_price < price_unit:
-                msg = _('The purchase price is superior than sales price')
-                raise UserError(msg)
+            #if sale_price < price_unit:
+            #    msg = _('The purchase price is superior than sales price')
+            #    raise UserError(msg)
 
 
 
