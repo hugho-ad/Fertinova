597e2c8c352da89640f905312449e7c90cf45a60
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-04-16 10:44:17 -0500

    [FIX] Correción en el ticket 035 para que a partir del botón de 'Confirmar Pedido' se valide si el precio de compra no sea superior al precio de venta de la propia compañía

--- fertinova_addons/models/purchase_inherited.py
+++ fertinova_addons/models/purchase_inherited.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 from odoo.addons import decimal_precision as dp
 from odoo.exceptions import UserError
 from odoo.tools.translate import _      
+
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
@@ -11,13 +11,16 @@ class PurchaseOrder(models.Model):
     ##########################################################
     # MODEL METHODS
     ##########################################################
-    @api.onchange('order_line')
-    def onchange_validate_price_unit(self):
+    @api.multi
+    def button_confirm(self):
         """This method obtain the purchase order id and its lines, in order to get
            sale prices of those products and avoiding to acquire when this cost
            is lesser than company wants to purchase from providers"""
+        result = super(PurchaseOrder, self).button_confirm()   
+
         #Obtain the current purchase order id:
-        purchase_order_id = self._origin.id  
+        purchase_order_id = self.id 
+        _logger.info('\n\n\n\n id: %s \n\n\n', purchase_order_id)       
         #Object of model 'purchase.order.line':
         purchase_order_obj = self.env['purchase.order'].browse(purchase_order_id)               
         #Retrieve the ids of the relatives lines belonging to the main purchase order:
@@ -27,14 +30,13 @@ class PurchaseOrder(models.Model):
         currency = purchase_order_obj.search([('id', '=', purchase_order_id)]).currency_id.id
 
         #Validate if the currency of purchase order is not Mexican Peso (MXN):
-        if purchase_order_id == None:
-            if currency != 33: #MXN has the id 33 in model res.currency                   
-                #If the currency is different to MXN, perform conversion:
-                sql_query = """SELECT rate FROM res_currency_rate WHERE id = %s;"""
-                self.env.cr.execute(sql_query, (currency,))
-                rate_aux = self.env.cr.fetchone()
-                rate = rate_aux[0] #Store rate or conversion factor
-                conversion_factor = 1 / rate             
+        if currency != 33: #MXN has the id 33 in model res.currency                   
+            #If the currency is different to MXN, perform conversion:
+            sql_query = """SELECT rate FROM res_currency_rate WHERE id = %s;"""
+            self.env.cr.execute(sql_query, (currency,))
+            rate_aux = self.env.cr.fetchone()
+            rate = rate_aux[0] #Store rate or conversion factor
+            conversion_factor = 1 / rate             
 
         #Validation in order to avoid purchases when price unit is lesser than sale price:
         for value in purchase_order_lines.ids:
@@ -56,6 +58,8 @@ class PurchaseOrder(models.Model):
             if sale_price < price_unit:
                 msg = _('The purchase price is superior than sales price')
                 raise UserError(msg)
+    
+        return result
 
 
 
