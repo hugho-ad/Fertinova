a33ad30899ec7a43fa35df172a8a1573b01d25cd
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-05-04 16:07:17 -0500

    [FIX] Correcciones en cálculos de Kardex

--- fertinova_addons/models/purchase_inherited.py
+++ fertinova_addons/models/purchase_inherited.py
@@ -5,40 +5,6 @@ from odoo.exceptions import UserError
 from odoo.tools.translate import _      
 
 
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-    
-    ##########################################################
-    # MODEL METHODS
-    ##########################################################
-    @api.multi
-    def button_confirm(self):
-        """This method obtain the purchase order id and its lines, in order to get
-           sale prices of those products and avoiding to acquire when this cost
-           is lesser than company wants to purchase from providers"""
-        result = super(PurchaseOrder, self).button_confirm()   
-                   
-        #Validate if the currency of purchase order is not Mexican Peso (MXN):
-        mxn = self.env.ref('base.MXN')
-        if self.currency_id != mxn.id:                  
-            #If the currency is different to MXN, perform conversion:
-            rate = mxn._get_conversion_rate(self.env.user.company_id.currency_id, self.currency_id, self.company_id, self.date_order)           
-
-        msg = ""
-        for value in self.order_line.filtered("product_id.product_tmpl_id.valid_price_unit"):
-            #Auxiliar variables:
-            price_unit = value.price_unit * rate
-            sale_price = value.product_id.list_price
-            #Validation in order to avoid purchases when price unit is lesser than sale price:
-            if sale_price <= price_unit:
-                msg += _('\nThe purchase price $%s is superior than sales price $%s\n') % (price_unit, sale_price)
-        
-        if not msg:
-            return result
-        
-        raise UserError(msg)
-
-
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
