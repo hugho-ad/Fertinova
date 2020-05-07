2be9d685fd5cafdbf11c0ec5021f99b29e9562e6
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-05-07 08:38:13 -0500

    [ERR] Cambios inconclusos aún del ticket 035

--- fertinova_addons/models/purchase_inherited.py
+++ fertinova_addons/models/purchase_inherited.py
@@ -6,6 +6,63 @@ from odoo.tools.translate import _
 
 
 #//////////////////////////////////////////////////////////////////////////////////////////////#
+#   TICKET 035    DEVELOPED BY SEBASTIAN MENDEZ    --     START
+#//////////////////////////////////////////////////////////////////////////////////////////////#
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    ##########################################################
+    # MODEL METHODS
+    ##########################################################
+    @api.multi
+    def button_confirm(self):
+        """This method obtain the purchase order id and its lines, in order to get
+        sale prices of those products and avoiding to acquire when this cost
+        is lesser than company wants to purchase from providers"""
+        #Call of Odoo Standard Method "button_confirm":
+        result = super(PurchaseOrder, self).button_confirm() 
+
+        #Instance of purchase order lines:
+        purchase_order_lines = self.env['purchase.order.line'].search([('id', '=', self.order_id.id)])
+        
+        #Validate if the currency of purchase order is not Mexican Peso (MXN):
+        mxn = self.env.ref('base.MXN')
+        if self.currency_id.id != mxn.id: 
+            #If the currency is different to MXN, perform conversion:
+            rate = self.env['res.currency.rate'].search([('id', '=', self.currency_id.id)]).rate
+            conversion_factor = 1 / rate
+        
+        msg = ""
+        #for value in self.order_line.filtered("product_id.product_tmpl_id.valid_price_unit"):
+        for value in purchase_order_lines.ids:
+            #Retrieve "sale price" from table 'product.product' considering product_id: 
+            product_id = self.env['purchase.order.line'].search([('id', '=', value)]).product_id.id
+            sale_price = self.env['product.template'].search([('id', '=', product_id)]).list_price 
+
+            #Conversion to Mexican Pesos (MXN) of sale price:
+            if self.currency_id != mxn.id: 
+                #It is necessary to calculate the new value of currency:  
+                sale_price = float(sale_price) * float(conversion_factor)
+
+            #Get price_unit of each line from purchase order line:
+            price_unit = self.env['purchase.order.line'].search([('id', '=', value)]).price_unit               
+
+            #Validation in order to avoid purchases when price unit is lesser than sale price:
+            if float(price_unit) > sale_price:
+                msg += _('\nThe purchase price $%s is superior than sales price $%s\n') % (price_unit, sale_price)
+            #If everything is OK, proceed with purchase order confirmation
+            if not msg:
+                return result
+            
+            raise UserError(msg)       
+#//////////////////////////////////////////////////////////////////////////////////////////////#
+#   TICKET 035    DEVELOPED BY SEBASTIAN MENDEZ    --     END
+#//////////////////////////////////////////////////////////////////////////////////////////////#
+
+
+
+
+#//////////////////////////////////////////////////////////////////////////////////////////////#
 #   TICKET 028    DEVELOPED BY SEBASTIAN MENDEZ    --     START
 #//////////////////////////////////////////////////////////////////////////////////////////////#
 class PurchaseOrderLine(models.Model):
@@ -33,4 +90,4 @@ class PurchaseOrderLine(models.Model):
                 rec.qty_to_receive = rec.product_qty - rec.qty_received
 #//////////////////////////////////////////////////////////////////////////////////////////////#
 #   TICKET 028    DEVELOPED BY SEBASTIAN MENDEZ    --     END
-#//////////////////////////////////////////////////////////////////////////////////////////////#               
+#//////////////////////////////////////////////////////////////////////////////////////////////#  
