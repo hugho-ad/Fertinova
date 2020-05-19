19606fec06cbf54d43510072f0baad5a02465ac7
CSE Sebastián Ayala Méndez <62626813+Sebastian-AM-Wobin@users.noreply.github.com>
2020-05-19 13:57:23 -0500

    Update __init__.py

--- fertinova_addons/models/__init__.py
+++ fertinova_addons/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from . import purchase_inherited
+from . import product_template_inherited
 from . import sale_inherited
-from . import stock_move_inherited 
+from . import stock_move_inherited
