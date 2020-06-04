69e37463ee3746ec213ff7d7638434008839ae40
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-06-04 15:04:17 -0500

    [ERR] Código provisional

--- fertinova_addons/models/account_report_followup_inherit.py
+++ fertinova_addons/models/account_report_followup_inherit.py
@@ -9,36 +9,16 @@ _logger = logging.getLogger(__name__)
 #//////////////////////////////////////////////////////////////////////////////////////////////#
 #   TICKET 116    DEVELOPED BY SEBASTIAN MENDEZ    --     START
 #//////////////////////////////////////////////////////////////////////////////////////////////#
-class AccountFollowupReport(models.AbstractModel):
-    _inherit = 'account.followup.report'
+class report_account_aged_receivable(models.AbstractModel):
+    _inherit = "account.aged.receivable"
 
-    #########################################################
-    # MODEL METHODS
-    #########################################################
-    def _get_columns_name(self, options):
-        """This method intends to add a new column into Account Payable Report"""
-        #Sample about what is inside in headers list:   
-        #headers = [{},
-        #           {'name': _('Date'), 'class': 'date', 'style': 'text-align:center; white-space:nowrap;'},
-        #           {'name': _('Due Date'), 'class': 'date', 'style': 'text-align:center; white-space:nowrap;'},
-        #           {'name': _('Source Document'), 'style': 'text-align:center; white-space:nowrap;'},
-        #           {'name': _('Communication'), 'style': 'text-align:right; white-space:nowrap;'},
-        #           {'name': _('Expected Date'), 'class': 'date', 'style': 'white-space:nowrap;'},
-        #           {'name': _('Excluded'), 'class': 'date', 'style': 'white-space:nowrap;'},
-        #           {'name': _('Total Due'), 'class': 'number o_price_total', 'style': 'text-align:right; white-space:nowrap;'}
-        #          ]     
-            
-        #Retrieve original list:
-        headers = super(AccountFollowupReport, self)._get_columns_name(options)
-        #Add new column of Invoice Date:
-        headers.insert(2, {'name': _('Invoice Date'), 'class': 'date', 'style': 'text-align:center; white-space:nowrap;'})
-
-        return headers
+    def _get_columns_name(self, options):    
+        columns = super()_get_columns_name(options)
+        columns.insert(4, {'name': _('Invoice Date'), 'class': 'date', 'style': 'text-align:center; white-space:nowrap;'})
+        return columns
     
-    
-    def _get_lines(self, options, line_id=None):        
-        """This method intends to add a new column into Account Payable Report"""
-        pass
+    def _get_lines(self, options, line_id=None):  
+        pass      
         
 #//////////////////////////////////////////////////////////////////////////////////////////////#
 #   TICKET 116    DEVELOPED BY SEBASTIAN MENDEZ    --     END
