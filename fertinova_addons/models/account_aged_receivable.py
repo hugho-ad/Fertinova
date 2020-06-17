2b45ad198c51e1abf2481f903146b95eb7208a24
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-06-17 16:07:25 -0500

    [IMP] Mejoras al ticket en archivo de traducciones

--- fertinova_addons/models/account_aged_receivable.py
+++ fertinova_addons/models/account_aged_receivable.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
+import logging
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 from odoo.tools.translate import _  
 
-import logging
 _logger = logging.getLogger(__name__)
 
 #//////////////////////////////////////////////////////////////////////////////////////////////#
